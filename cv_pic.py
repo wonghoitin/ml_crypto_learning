f1da5b8acfaab9971e765d6219b4c15ecbe16b6b
wonghoitin <106225274+wonghoitin@users.noreply.github.com>
2022-08-01 14:35:33 +0800

    Update cv_pic.py

--- cv_pic.py
+++ cv_pic.py
@@ -11,17 +11,15 @@ import numpy
 # In[2]:
 
 
-class cv_pic():
+def pic_in(in_path, out_path):
+    target = cv.imread(in_path)
+    target.tofile(out_path)
+    return target.shape, target
 
-    def pic_in(in_path, out_path):
-        target = cv.imread(in_path)
-        target.tofile(out_path)
-        return target.shape, target
-
-    def pic_out(in_path, win, shape):
-        data_out = numpy.fromfile(in_path, dtype = numpy.uint8)
-        final = data_out.reshape(shape[0], shape[1], shape[2])
-        cv.namedWindow(win, cv.WINDOW_NORMAL)
-        cv.imshow(win, final)
-        cv.waitKey(0)
+def pic_out(in_path, win, shape):
+    data_out = numpy.fromfile(in_path, dtype = numpy.uint8)
+    final = data_out.reshape(shape[0], shape[1], shape[2])
+    cv.namedWindow(win, cv.WINDOW_NORMAL)
+    cv.imshow(win, final)
+    cv.waitKey(0)
 
